1abf2d583fd398eec07f14a036730a5922b6d0d3
Casey Law <caseyjlaw@gmail.com>
2023-03-30 11:01:20 -0400

    updates every 24th test. better logging

--- comet/plugins/eventwriter.py
+++ comet/plugins/eventwriter.py
@@ -10,6 +10,7 @@ from slack_sdk.errors import SlackApiError
 
 import voeventparse
 from ovro_alert import alert_client
+import time
 
 from zope.interface import implementer
 from twisted.plugin import IPlugin
@@ -69,6 +70,8 @@ class EventWriter(object):
 
     def __init__(self):
         self.directory = os.getcwd()
+        self.testcount = 0
+        self.starttime = time.time()
 
     # When the handler is called, it is passed an instance of
     # comet.utility.xml.xml_document.
@@ -80,14 +83,22 @@ class EventWriter(object):
             os.makedirs(self.directory)
 
         with event_file(event.element.attrib["ivorn"], self.directory) as f:
-            log.debug("Writing to %s" % (f.name,))
+            log.info("Writing to %s" % (f.name,))
             f.write(event.raw_bytes.decode(event.encoding))
 
         # create voevent
         log.info("Creating voevent")
         voevent = voeventparse.loads(event.raw_bytes) 
-        self.update_relay(voevent)
-        self.update_slack(voevent)
+        role = voevent.get('role')
+
+        # send all non-tests, plus ever 24th test
+        if (role != 'test') or (self.testcount % 24):
+            self.update_relay(voevent)
+            self.update_slack(voevent)
+
+        if role == 'test':
+            log.info("Incrementing test counter")
+            self.testcount += 1
 
     def get_options(self):
         return [("directory", self.directory, "Directory in which to save events")]
@@ -100,7 +111,7 @@ class EventWriter(object):
         """ parse VOEvent file and send a message to slack 
         """
 
-        log.info("Sending to slack")
+        log.info("Parsing event for slack")
 
         client = WebClient(token=SLACK_TOKEN)
 
@@ -113,21 +124,26 @@ class EventWriter(object):
             message = f"CHIME/FRB VOEvent Received: \n TOA: {toa} \n Event Position: {position} \n DM: {dm}",
         else:
             date = voevent.Who.find("Date")
-            description = voevent.What.find("Description")
-            message = f"Parsed {role} event at {date} described as: {description}"
+            if self.testcount > 0:
+                testrate = ((time.time()-self.starttime)/3600)/self.testcount
+                message = f"CHIME/FRB test report at {date}: received {self.testcount} events since start at rate of {testrate:.1f} per hour."
+            else:
+                message = f"CHIME/FRB test report at {date}: received first test event and will report every 24th event."
+
+        log.info("Sending to slack")
 
         # Post to slack
         try:
             response = client.chat_postMessage(channel="#candidates", text=message, icon_emoji = ":zap:")
-            print(response)
+            log.info(response)
         except SlackApiError as e:
-            print("Error sending message: {}".format(e))
+            log.error("Error sending message: {}".format(e))
 
     def update_relay(self, voevent):
         """ parse VOEvent file and send info to relay server
         """
 
-        log.info("Sending to relay")
+        log.info("Parsing event for relay")
 
         dsac = alert_client.AlertClient('dsa')
 
@@ -142,7 +158,7 @@ class EventWriter(object):
             description = voevent.What.find("Description").text
             args = {"role": role, "date": date, "description": description}
 
-        log.info("dsac.set()")
+        log.info("Set to relay")
         dsac.set("CHIME FRB", args=args)
 
 # This instance of the handler is what actually constitutes our plugin.
